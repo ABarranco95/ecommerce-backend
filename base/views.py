9220f0547117c2654393ecfda1bd91a8ab1d3339
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-02 23:07:49 -0800

    [urls.js] create route for specific product

--- base/views.py
+++ base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
 
 # Create your views here.
 
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/products/',
@@ -18,4 +23,18 @@ def getRoutes(request):
         '/api/products/delete/<id>/',
         '/api/products/update/<id>/',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
