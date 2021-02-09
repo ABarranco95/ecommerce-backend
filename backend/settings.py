dd7c4fd66640edda6146dd486d3eb33cddcdc9b6
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-09 15:12:47 -0800

    Set up and install JSON web tokens

--- backend/settings.py
+++ backend/settings.py
@@ -45,11 +45,12 @@ INSTALLED_APPS = [
 
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthemAuthentication',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 MIDDLEWARE = [
