3bf96ff530bd8d74021c2d70ca0952cd1d5328fb
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-10 20:02:03 -0800

    add base_dir for static

--- backend/settings.py
+++ backend/settings.py
@@ -171,7 +171,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static'.
+    BASE_DIR / 'frontend/build/static'
 ]
 
 MEDIA_ROOT = 'static/images'
