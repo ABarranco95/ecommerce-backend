e9a65553a0045904ccde20252bd2abe59e1ebef9
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-10 17:05:20 -0800

    Create update profile endpoint

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -49,6 +49,28 @@ def registerUser(request):
 
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+
+    data = request.data
+
+    user.first_name = data['first_name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password ==make_password(data['password'])
+
+    user.save()
+
+
+    return Response(serializer.data)
+
+
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
