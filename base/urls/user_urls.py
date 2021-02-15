0d49a4c3ca336d32ed1173b090548e9082672d81
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-14 16:42:20 -0800

    add delete route to remove user from db

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
 
     path('profile/', views.getUserProfile, name='users-profile'),
     path('profile/update/', views.updateUserProfile, name='user-profile-update'),
-
-
     path('', views.getUsers, name='users'),
+
+
+    path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 
 ]
