5ae49dfdecc12d476669e10a4fb44050587b4841
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-16 18:32:09 -0800

    create review endpoint

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('create/', views.createProduct, name='product-create'),
     path('upload/', views.uploadImage, name='image-upload'),
     
-    path('<str:pk>', views.getProduct, name='product'),
+    path('<str:pk>/reviews', views.createProductReview, name='create-review'),
+    path('<str:pk>/', views.getProduct, name='product'),
 
     path('update/<str:pk>/', views.updateProduct, name='product-update'),
     path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
