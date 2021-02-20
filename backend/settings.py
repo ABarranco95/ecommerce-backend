c98784dce7d653ff30f645902ae476891b893691
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-19 17:59:05 -0800

    debug false

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_iok1w1d038qb%lt_+w=g@f-tl=1swrbbsk6*q^-7@pu&akee&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
